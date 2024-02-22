01ee0d4f7f2fa37363823fafae861809eb8bc860
Nejla Skelic <nejla.kubat99@gmail.com>
2024-02-22 11:10:27 +0100

    Multiple questions
    Working with array of questions displayed one at a time.

--- app.js
+++ app.js
@@ -1,28 +1,57 @@
 (function main () {
     const statement = document.getElementById("statement");
     const optionButtons = document.querySelector("#options").children;
+    const nextButton = document.getElementById("next");
     const explanation = document.getElementById("explanation");
 
-    const fact = {
-        statement: "Guess if this is true",
-        answer: true,
-        explanation: "Explanation"
-    }
+    let currentQuestion = 0;
 
-    statement.textContent = fact.statement;
+    const facts = [
+        {
+            statement: "Question 1",
+            answer: true,
+            explanation: "Explanation"
+        },
+        {
+            statement: "Question 2",
+            answer: false,
+            explanation: "Explanation"
+        },
+        {
+            statement: "Question 3",
+            answer: false,
+            explanation: "Explanation"
+        },
+        {
+            statement: "Question 4",
+            answer: true,
+            explanation: "Explanation"
+        },
+        {
+            statement: "Question 5",
+            answer: false,
+            explanation: "Explanation"
+        }
+    ]
+
+    statement.textContent = facts[currentQuestion].statement;
 
     function disable(button) {
         button.setAttribute("disabled", "")
     }
 
+    function enable(button) {
+        button.removeAttribute("disabled")
+    }
+
     function isCorrect(guessString) {
-        return guessString === fact.answer.toString();
+        return guessString === facts[currentQuestion].answer.toString();
     }
 
     function f(e) {
         const button = e.target;
         console.log("clicked");
-        explanation.textContent = fact.explanation;
+        explanation.textContent = facts[currentQuestion].explanation;
         disable(button);
         if(isCorrect(button.textContent)) { 
             button.style.color = "green";
@@ -33,7 +62,7 @@
     for(let button of optionButtons) {
         button.addEventListener("click", (e) => {
             console.log("clicked");
-            explanation.textContent = fact.explanation;
+            explanation.textContent = facts[currentQuestion].explanation;
             for(let button of optionButtons) {
                 disable(button);
             }
@@ -41,5 +70,24 @@
             isCorrect(button.textContent) ? button.classList.add("correct") : button.classList.add("incorrect");
         })
     };
+
+    nextButton.addEventListener("click", (e) => {
+        console.log("next clicked");
+        if(currentQuestion<facts.length-1) {
+            currentQuestion++;
+            for(let button of optionButtons) {
+                enable(button);
+                button.classList.remove("correct");
+                button.classList.remove("incorrect");
+            }
+            statement.textContent = facts[currentQuestion].statement;
+            explanation.textContent="";
+        }
+        else {
+            disable(nextButton);
+            explanation.textContent="No more questions!";
+        }
+        
+    })
     
 })();
